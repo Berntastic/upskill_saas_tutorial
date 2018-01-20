f4df33cfe8e0f100ffd1c3567f571f87c44080ef
First Last <bengraymusic@gmail.com>
2018-01-20 04:00:33 +0000

    added contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
-  get 'Contact-Us', to: 'contacts#new'
+  resources :contacts, only: :create
+  get 'Contact-Us', to: 'contacts#new', as: 'new_contact'
 end
